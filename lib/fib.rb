011bdcd3f96e5c13fcae5848a1e08c07cb12c177
Matt Simpson <matt.simpson3@gmail.com>
2012-09-11 21:44:38 -0500

    Add alternative solution

--- lib/fib.rb
+++ lib/fib.rb
@@ -1,15 +1,13 @@
 class Fib
   def values
-    @values = []
-    6.times do |i|
-      if @values.empty?
-        @values << 1
-      elsif @values.size == 1
-        @values << 2
+    (0..6).inject([]) do |vals, n|
+      vals << if vals.size < 2
+        vals.size + 1
       else
-        @values << @values[i-2] + @values[i-1]
+        vals[n-2] + vals[n-1]
       end
+      vals
     end
-    @values
   end
 end
+
