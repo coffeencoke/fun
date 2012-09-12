964dd7433c40483761cdb971b69bcbe47036e0e8
Matt Simpson <matt.simpson3@gmail.com>
2012-09-11 22:15:35 -0500

    Update the fib algorithm to be more accurate, more readable, but less performant than the other versions

--- lib/fib.rb
+++ lib/fib.rb
@@ -1,13 +1,17 @@
+# Fib
+# 
+# Generates the values of the Fibonacci Sequence for a given number of sequences
+# http://en.wikipedia.org/wiki/Fibonacci_number
+#
+#   1.9.3p194 :010 > Fib.values(20)
+#   => [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
+# 
 module Fib
   def self.values(limit=9)
-    [0,1] + (0..limit-2).inject([]) do |vals, n|
-      vals << value(vals[n-2], vals[n-1])
-    end
+    (0..limit).map{|a| value(a) }
   end
 
-  def self.value(back_two_values, back_one_value)
-    back_two_values ||= 0
-    back_one_value ||= 1
-    back_two_values + back_one_value
+  def self.value(n)
+    n < 2 ? n : value(n-1) + value(n-2)
   end
 end
