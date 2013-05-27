050316aa130a9131ec8d09734a9ae5503739acdb
Matt Simpson <matt.simpson3@gmail.com>
2013-05-26 21:49:13 -0500

    Update quick sort

--- lib/quick_sort.rb
+++ lib/quick_sort.rb
@@ -1,34 +1,43 @@
 class QuickSort
-  attr_reader :array
+  attr_accessor :array
 
   def initialize(array)
-    @array = array
+    self.array = array
   end
 
   def call
-    sort(array)
-  end
-
-  def sort(sub_array)
-    sub_array.length <= 1 ? sub_array : quick_sort(sub_array)
+    quick_sort(1, array.size - 1)
+    array
   end
 
   private
 
-  def quick_sort(sub_array)
-    pivot = sub_array.pop
+  def quick_sort(left, right)
+    if left < right
+      pivot_index = left + 1
+      new_pivot_index = partition(left, right, pivot_index)
+      quick_sort(left, new_pivot_index - 1)
+      quick_sort(new_pivot_index + 1, right)
+    end
+  end
 
-    less = []
-    greater = []
+  def partition(left, right, pivot_index)
+    pivot_value = array[pivot_index]
+    right_value = array[right]
+    array[right] = pivot_value
+    array[pivot_index] = right_value
 
-    sub_array.each do |x|
-      if x <= pivot
-        less << x
-      else
-        greater << x
+    store_index = left
+    (left..right-1).each do |i|
+      i_val = array[i]
+      if i_val < pivot_value
+        array[i] = array[store_index]
+        store_index += 1
       end
     end
 
-    sort(less) + [pivot] + sort(greater)
+    array[store_index] = array[right]
+
+    store_index
   end
 end
