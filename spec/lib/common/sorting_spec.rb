ebe6a7575f934668dc437bc22245817b7ee90a8d
Matt Simpson <matt.simpson3@gmail.com>
2013-06-15 18:26:11 -0500

    Consolidate sorting specs

---
+++ spec/lib/common/sorting_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../../../lib/common/insertion_sort'
+require_relative '../../../lib/quick_sort'
+
+ARRAY = [1,6,4,5,7,9,4,22,34,6,2,1,423,12,5,8,32,2]
+EXPECTED = ARRAY.sort
+
+module Common
+  describe InsertionSort do
+    subject { described_class.sort(*ARRAY) }
+
+    it { should == EXPECTED }
+  end
+end
+
+
+describe QuickSort do
+  subject { QuickSort.new ARRAY }
+
+  it 'can sort an array' do
+    subject.call.should == EXPECTED
+  end
+end
+
