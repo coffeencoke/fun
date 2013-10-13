5425f1d563cb94b30f16cb4a132bcfa89154b3f1
Matt Simpson <matt.simpson3@gmail.com>
2013-10-12 21:19:07 -0500

    Remove some loops, and conversions

--- lib/problems/no_order_of_operations.rb
+++ lib/problems/no_order_of_operations.rb
@@ -1,10 +1,10 @@
 module NoOrderOfOperations
   def self.evaluate(expression)
-    numbers = expression.split(/[-+*]/).map(&:to_i)
-    operators = expression.split(/\d/).reject{|a| a == '' }
-    total = numbers[0]
+    numbers = expression.split(/[-+*]/)
+    operators = expression.split(/\d/)
+    total = numbers[0].to_i
     numbers[0..-2].each_with_index do |number, i|
-      total = total.send(operators[i], numbers[i+1])
+      total = total.send(operators[i+1], numbers[i+1].to_i)
     end
     total
   end
