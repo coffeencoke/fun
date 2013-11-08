57ddce63f77b6c3f1d2cc6a08395a03000412a8c
Matt Simpson <matt.simpson3@gmail.com>
2013-11-07 22:44:40 -0600

    minor tweak

--- test.rb
+++ test.rb
@@ -32,14 +32,14 @@ n4 = Node.new(7)
 n3 = Node.new(4)
 n2 = Node.new(2)
 n1 = Node.new(3, n2, n3)
-root = Node.new(5)
+root = Node.new(5, n1, n4)
 
 def binary_search_tree?(root, max=1/0.0, min=-1/0.0)
   return true if root.nil?
 
   root_cond = root.value < max && root.value > min
-  left_cond = root.left.nil? || binary_search_tree?(root.left, root.value, min)
-  right_cond = root.right.nil? || binary_search_tree?(root.right, max, root.value)
+  left_cond = binary_search_tree?(root.left, root.value, min)
+  right_cond = binary_search_tree?(root.right, max, root.value)
 
   root_cond && left_cond && right_cond
 end
